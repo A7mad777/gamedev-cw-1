09bbab383ec5d1387c5b75b6cdd7936ccce4265e
Ahmad <a7mad7ussain777@gmail.com>
2020-12-20 19:16:52 +0300

    Ahmad-M1-67
    CW-1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ahmad_M1_67
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string chractername = "DeathKnight";
+            string superpower1 = "Iron Fist";
+            string superpower2 = "Death Strik";
+            int age = 23;
+            float speed = 2.5f;
+
+            Console.WriteLine("My Name is {0} and my power is {1} and {2}. i'm {3} old and my speed is {4}", chractername, superpower1, superpower2, age, speed);
+        }
+    }
+}

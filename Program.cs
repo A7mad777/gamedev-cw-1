93d01324a9bcf7b0720c4de7ae04b752fdc345b6
Ahmad <a7mad7ussain777@gmail.com>
2020-12-21 17:44:10 +0300

    CW-2

--- Program.cs
+++ Program.cs
@@ -12,7 +12,17 @@ namespace Ahmad_M1_67
             int age = 23;
             float speed = 2.5f;
 
+
             Console.WriteLine("My Name is {0} and my power is {1} and {2}. i'm {3} old and my speed is {4}", chractername, superpower1, superpower2, age, speed);
+
+            double herohight = 176;
+            string heroname = "Ahmad";
+            string herosuperpower1 = "KillerStrike";
+            string herosuperpower2 = "SuperKik";
+            int heroage = 19;
+
+            Console.WriteLine(age - heroage);
+
         }
     }
 }
